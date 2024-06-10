bfea94b94fcae5fd81d1d5c1417d70d921c18aa4
M. Juniko Dwi Putra <official.juniko@gmail.com>
2024-06-11 00:20:44 +0700

    sitemap slug page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,26 @@
-// next-sitemap.config.js
+const fetchDynamicPaths = async () => {
+  const fetch = (await import('node-fetch')).default; 
+  const response = await fetch(`${process.env.BASE_API_URL}/article`); // Use the environment variable
+  const data = await response.json();
 
-/** @type {import('next-sitemap').IConfig} */
+  return data.data.map(item => ({
+    loc: `https://www.sarolangunkito.com/${item.slug}`, // Adjust path according to your slug structure
+    lastmod: new Date().toISOString(), // You can adjust the date format or source
+  }));
+};
+
 module.exports = {
-    siteUrl: 'https://www.sarolangunkito.com', // Replace with your site's URL
-    generateRobotsTxt: true, // Generate robots.txt file
-    // Other options can be added here
-  }
-  
+  siteUrl: 'https://www.sarolangunkito.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000, // Adjust if you expect more URLs to split the sitemap into multiple files
+  changefreq: 'daily', // Set according to your update frequency
+  priority: 0.7, // Adjust priority for your pages
+  generateIndexSitemap: false, // Generates a sitemap index file
+  async additionalPaths(config) {
+    const dynamicPaths = await fetchDynamicPaths();
+    return dynamicPaths.map(path => ({
+      loc: path.loc,
+      lastmod: path.lastmod,
+    }));
+  },
+};
